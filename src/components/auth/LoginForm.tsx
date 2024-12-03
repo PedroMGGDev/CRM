2b5717f4f80b436bfcb39282990bdb304ddc61d5
Pedro Miguel Gomes Guerrero <123781972+PedroMGGDev@users.noreply.github.com>
2024-12-03 13:46:03 -0300

    Update LoginForm.tsx

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -139,7 +139,7 @@ export default function LoginForm() {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Entre na sua conta
+            Entre na sua co
           </h2>
         </div>
         {errorMessage && (
